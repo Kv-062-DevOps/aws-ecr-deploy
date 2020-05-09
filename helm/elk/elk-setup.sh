78e7cfc02801c582abde8a826aba3044fcf0e85d
nigth <ritzio@mail.ru>
2020-05-10 01:46:42 +0300

    changed CPU and RAM to minimum and added SH script

---
+++ helm/elk/elk-setup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# https://itnext.io/deploy-elastic-stack-on-kubernetes-1-15-using-helm-v3-9105653c7c8
+NAME=$2
+NS=log
+CHART=elastic/${NAME}
+VERSION=v7.6.1
+VALUES=${NAME}/values.yaml
+RELEASE=$(helm ls | awk '{print $1}' | grep ${NAME})
+
+display_usage() {
+	echo -e "\nThis script must be run with 2 parameters"
+	echo -e "\nUsage:\n$0 [install|update] [elasticsearch|kibana|metricbeat]"
+	echo -e "\n Example:\n$0 install kibana \n"
+	}
+
+if [[  $# -le 1 ||  $# == "-h" ]]
+then
+  display_usage
+  exit 1
+fi
+
+
+case $1 in
+install)
+		helm install ${CHART} --namespace ${NS} -f ${VALUES} --version ${VERSION} --generate-name
+		#helm install --debug --dry-run --name ${NAME} --namespace ${NS} -f ${NAME}.yaml ${CHART} > ${NAME}-debug.yaml
+		echo "Installed ${NAME}"
+		;;
+update)
+		helm upgrade ${RELEASE} ${CHART} --namespace ${NS} -f ${VALUES} --version ${VERSION}
+		echo "Updated ${NAME}"
+		;;
+*)
+		display_usage
+		;;
+esac
+
+
