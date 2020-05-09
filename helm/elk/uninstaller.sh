312ed315d554a7327e5636fa76f26366e13253ff
nigth <ritzio@mail.ru>
2020-05-10 02:24:04 +0300

    removed manually

--- helm/elk/uninstaller.sh
+++ helm/elk/uninstaller.sh
@@ -2,8 +2,10 @@
 
 NS=log
 
-helm uninstall filebeat
-helm uninstall metricbeat
-helm uninstall kibana
-helm uninstall elasticsearch
+helm uninstall filebeat-1589065779 --namespace ${NS}
+helm uninstall metricbeat-1589065690 --namespace ${NS}
+helm uninstall kibana-1589065558 --namespace ${NS}
+helm uninstall elasticsearch-1589065379 --namespace ${NS}
+
+kubectl get all --namespace ${NS}
 
