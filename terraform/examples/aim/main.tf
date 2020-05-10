e03b2b739851cce72cfd1402091337f62d21d788
nigth <ritzio@mail.ru>
2020-05-10 17:27:27 +0300

    https://linux-notes.org/rabota-s-aws-iam-i-terraform-v-unix-linux/

---
+++ terraform/examples/aim/main.tf
@@ -0,0 +1,39 @@
+#
+# Try to use this with KOPS
+#
+terraform {
+  required_version = "> 0.9.0"
+}
+provider "aws" {
+    region  = "us-east-1"
+    profile = "default"
+    # access_key = "${var.aws_access_key}"
+    # secret_key = "${var.aws_secret_key}"
+}
+module "aim" {
+    source                          = "../../modules/iam"
+    name                            = "TEST-AIM"
+    region                          = "us-east-1"
+    environment                     = "PROD"
+
+    aws_iam_role-principals         = [
+        "ec2.amazonaws.com",
+    ]
+    aws_iam_policy-actions           = [
+        "cloudwatch:GetMetricStatistics",
+        "logs:DescribeLogStreams",
+        "logs:GetLogEvents",
+        "elasticache:Describe*",
+        "rds:Describe*",
+        "rds:ListTagsForResource",
+        "ec2:DescribeAccountAttributes",
+        "ec2:DescribeAvailabilityZones",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeVpcs",
+  ]
+  # Enabling cross account role
+  enable_crossaccount_role = "false"
+  cross_acc_principal_arns = ["222222222222222","arn:aws:iam::333333333333:user/test"]
+  cross_acc_policy_arns    = ["arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser", "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"]
+}
+
